10852cc534a159773b06b5aeee822b8e06451330
amnox <khalid_aman@ymail.com>
2018-08-30 01:12:41 +0530

    Redux Store and Package List Update

--- code/App.js
+++ code/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import middleware from './middleware';
+import reducer from './reducers';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store = { createStore(reducer,middleware) } >
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
+      </Provider>
     );
   }
 }
